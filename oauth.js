014db021b72503710a4bcba32674c9fac602ec44
Gouthamve <cs14btech11014@iith.ac.in>
2015-07-09 14:49:04 +0530

    Add registrations page
    Signed-off-by: Gouthamve <cs14btech11014@iith.ac.in>

--- oauth.js
+++ oauth.js
@@ -1,12 +1,12 @@
 var ids = {
 	facebook: {
-		clientID: '',
-		clientSecret: '',
+		clientID: 'f',
+		clientSecret: 'f',
 		callbackURL: 'http://127.0.0.1:3001/auth/facebook/callback'
 	},
 	google: {
-		clientID: '',
-		clientSecret: '',
+		clientID: 'f',
+		clientSecret: 'f',
 		callbackURL: 'http://127.0.0.1:3001/auth/google/callback'
 	}
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user')
+
+var mongoose = require('mongoose');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
@@ -8,5 +12,13 @@ router.get('/', function(req, res, next) {
     user: req.user
   });
 });
+
+router.get('/reg', function(req, res, next) {
+  User.find().exec(function(err, users) {
+    res.render('registrations', {
+      people: users
+    })
+  })
+})
 
 module.exports = router;
